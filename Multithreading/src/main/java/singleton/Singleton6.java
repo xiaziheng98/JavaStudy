f0db20fd6222c899147ca453f0844dfed9d6886b
xiaziheng <1192939559@qq.com>
2020-07-20 15:39:16 +0800

    singleton6 new instance

--- Multithreading/src/main/java/singleton/Singleton6.java
+++ Multithreading/src/main/java/singleton/Singleton6.java
@@ -5,6 +5,10 @@ package singleton;
  */
 public class Singleton6 {
 
+    // 使用volatile是因为创建对象不是原子操作，经历了三个步骤：
+    // 1.
+    // 2.
+    // 3.
     private volatile static Singleton6 instance;
 
     private Singleton6() {
